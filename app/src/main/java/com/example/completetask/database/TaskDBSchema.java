7e07022ce861d5bdb2d854268fa1c4e4f8b41887
mohammadreza <you@example.com>
2019-09-21 10:25:13 -0700

    +add four tables in TaskDBSchema class

--- app/src/main/java/com/example/completetask/database/TaskDBSchema.java
+++ app/src/main/java/com/example/completetask/database/TaskDBSchema.java
@@ -3,18 +3,66 @@ package com.example.completetask.database;
 public class TaskDBSchema {
     public static final String NAME = "task.db";
 
-    public static final class Task {
-        public static final String NAME = "Task";
+    public static final class Doing {
+        public static final String NAME = "Doing";
 
         public static final class Cols {
             public static final String _ID = "_id";
             public static final String UUID = "uuid";
             public static final String TITLE = "title";
             public static final String DATE = "date";
-            public static final String SOLVED = "solved";
-            public static final String ADDRES = "addres";
-            public static final String PHONENUMBER = "phonenumber";
+            public static final String TIME = "time";
             public static final String DISCRIPTION = "discription";
+            public static final String USERNAME = "username";
+            public static final String DONECHECKBOX = "donecheckbox";
+            public static final String DOINGCHECKBOX = "doingcheckbox";
+            public static final String TODOCHECKBOX = "todocheckbox";
+
+        }
+    }
+    public static final class Done {
+        public static final String NAME = "Done";
+
+        public static final class Cols {
+            public static final String _ID = "_id";
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String TIME = "time";
+            public static final String DISCRIPTION = "discription";
+            public static final String USERNAME = "username";
+            public static final String DONECHECKBOX = "donecheckbox";
+            public static final String DOINGCHECKBOX = "doingcheckbox";
+            public static final String TODOCHECKBOX = "todocheckbox";
+
+        }
+    }
+    public static final class ToDo {
+        public static final String NAME = "ToDo";
+
+        public static final class Cols {
+            public static final String _ID = "_id";
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String TIME = "time";
+            public static final String DISCRIPTION = "discription";
+            public static final String USERNAME = "username";
+            public static final String DONECHECKBOX = "donecheckbox";
+            public static final String DOINGCHECKBOX = "doingcheckbox";
+            public static final String TODOCHECKBOX = "todocheckbox";
+
+        }
+    }
+    public static final class User {
+        public static final String NAME = "User";
+
+        public static final class Cols {
+            public static final String _ID = "_id";
+            public static final String UUID = "uuid";
+            public static final String USERNAME = "username";
+            public static final String PASSWORD = "password";
+
         }
     }
 
